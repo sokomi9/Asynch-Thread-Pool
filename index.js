0722258a57dba2185f8e77141348495515b9f0b0
Abiud Sokomi <abiudsokomi@gmail.com>
2024-01-17 12:53:43 +0300

    my commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const crypto = require("node:crypto");
+
+process.env.UV_THREADPOOL_SIZE = 16;
+const MAX_CALLS = 15;
+
+const start = Date.now();
+for (let i = 0; i<MAX_CALLS; i++){
+    crypto.pbkdf2("password", "salt", 100000, 512, "sha512", ()=> {
+        console.log(`Hash: ${i + 1}`, Date.now() - start);
+    });
+}
+console.log("Has: ", Date.now() - start);
